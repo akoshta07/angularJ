c561ea72ce0627557d3ed9fae29f8655992392ce
akash <akoshta16@gmail.com>
2018-01-01 13:15:15 +0530

    Template Froms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import{ PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -46,6 +48,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   providers: [DishService,
     PromotionService,
     LeaderService,],
+    entryComponents:[
+      LoginComponent
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
