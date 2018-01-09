ff3d2bce82e42152e65fec20fcddb2e497cc7e5e
akash <akoshta16@gmail.com>
2018-01-09 13:32:24 +0530

    HTTP Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,9 +21,13 @@ import { ContactComponent } from './contact/contact.component';
 import { DishService } from './services/dish.service';
 import{ PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service'
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
+
+import {baseURL} from './shared/baseurl';
+
 
 @NgModule({
   declarations: [
@@ -49,7 +53,9 @@ import { LoginComponent } from './login/login.component';
   ],
   providers: [DishService,
     PromotionService,
-    LeaderService,],
+    LeaderService,
+    {provide: 'BaseURL', useValue: baseURL},
+    ProcessHTTPMsgService],
     entryComponents:[
       LoginComponent
     ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 import { DishService} from '../services/dish.service';
@@ -18,7 +18,7 @@ export class HomeComponent implements OnInit {
   leader : Leader;
   constructor(private disheservice: DishService,
     private promotionservice: PromotionService,
-    private leaderservice: LeaderService) { }
+    private leaderservice: LeaderService,@Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
     this.disheservice.getFeaturedDish().subscribe(dish => this.dish = dish);
