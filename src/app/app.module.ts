0fc874f608927fb6c2e202e6c752a92ae4e36b5c
akash <akoshta16@gmail.com>
2018-01-10 12:53:46 +0530

    REST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,9 @@ import { LoginComponent } from './login/login.component';
 
 import {baseURL} from './shared/baseurl';
 
+import { RestangularModule, Restangular } from 'ngx-restangular';
+import { RestangularConfigFactory } from './shared/restConfig';
+ 
 
 @NgModule({
   declarations: [
@@ -49,7 +52,8 @@ import {baseURL} from './shared/baseurl';
     MaterialModule,
     FlexLayoutModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RestangularModule.forRoot(RestangularConfigFactory)
   ],
   providers: [DishService,
     PromotionService,
